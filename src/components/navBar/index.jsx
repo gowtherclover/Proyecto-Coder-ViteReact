46e033dfadfb689bdba6d1ff327b6aa4d7b5c02d
Feco-Enzo <96enzoaguilar@gmail.com>
2023-03-01 11:53:11 -0300

    3er commit: instalando MUI para probar

--- src/components/navBar/index.jsx
+++ src/components/navBar/index.jsx
@@ -31,25 +31,6 @@ export default function Barra(){
                 </Navbar.Collapse>
             </Container>
         </Navbar>
-        /* <Navbar className='background' expand="md">
-            <Container fluid>
-                <Navbar.Brand href="#home">MALKA</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
-                        <Nav.Link><CartWidget cantidad="3"/></Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar> */
+        
     )
 }
